b741e99ee112e22fe8d0a07ead1d1ac19b3a8702
Swetha-3105 <108280698+Swetha-3105@users.noreply.github.com>
2023-07-02 00:08:26 +0530

    Add files via upload

---
+++ tkdict.py
@@ -0,0 +1,56 @@
+# Import Required Library
+from tkinter import *
+from PyDictionary import PyDictionary
+from PIL import Image, ImageTk
+
+# Create Objects
+dictionary = PyDictionary()
+window = Tk()
+
+# Set geometry
+window.geometry("400x400")
+
+
+def dict():
+	meaning.config(text=dictionary.meaning(word.get())['Noun'][0])
+	synonym.config(text=dictionary.synonym(word.get()))
+	antonym.config(text=dictionary.antonym(word.get()))
+
+
+# Add Labels, Button and Frames
+Label(window, text="Dictionary", font=(
+	"Helvetica 20 bold"), fg="Green").pack(pady=10)
+
+# Frame 1
+frame = Frame(window)
+Label(frame, text="Type Word", font=("Helvetica 15 bold")).pack(side=LEFT)
+word = Entry(frame, font=("Helvetica 15 bold"))
+word.pack()
+frame.pack(pady=10)
+
+# Frame 2
+frame1 = Frame(window)
+Label(frame1, text="Meaning:- ", font=("Helvetica 10 bold")).pack(side=LEFT)
+meaning = Label(frame1, text="", font=("Helvetica 10"))
+meaning.pack()
+frame1.pack(pady=10)
+
+# Frame 3
+frame2 = Frame(window)
+Label(frame2, text="Synonym:- ", font=("Helvetica 10 bold")).pack(side=LEFT)
+synonym = Label(frame2, text="", font=("Helvetica 10"))
+synonym.pack()
+frame2.pack(pady=10)
+
+# Frame 4
+frame3 = Frame(window)
+Label(frame3, text="Antonym:- ", font=("Helvetica 10 bold")).pack(side=LEFT)
+antonym = Label(frame3, text="", font=("Helvetica 10"))
+antonym.pack(side=LEFT)
+frame3.pack(pady=10)
+
+b1=Button(window, text="Submit", font=("Helvetica 15 bold"), command=dict)
+b1.pack()
+
+# Execute Tkinter
+window.mainloop()
